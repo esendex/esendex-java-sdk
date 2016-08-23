94a5163ba66e3e3d9bdf5ca7b0671e92012a926b
George 'Ice Cold' Frost and Steven Stinson <development+GeorgeFrost+slstinson@esendex.com>
2016-08-23 14:25:59 +0100

    Fix error in verification of duplicate meta data keys in surveys

--- src/main/java/esendex/sdk/java/service/impl/SurveysServiceImpl.java
+++ src/main/java/esendex/sdk/java/service/impl/SurveysServiceImpl.java
@@ -46,7 +46,7 @@ public class SurveysServiceImpl
             for (String string : alreadyKeys) {
                 if(string == item.getName())
                     throw new EsendexException("MetaData fields collection cannot contain duplicate fields");
-                alreadyKeys.add(string);
+                alreadyKeys.add(item.getName());
             }
         }
     }
